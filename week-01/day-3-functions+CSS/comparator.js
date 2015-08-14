0e02dcb7ee2d37b0e825eb02c55350c2df0e65bb
humberto J calero <Humberto@humbertos-MacBook-Pro.local>
2015-08-14 09:51:31 -0700

    Thursday night work

--- week-01/day-3-functions+CSS/comparator.js
+++ week-01/day-3-functions+CSS/comparator.js
@@ -1,3 +1,36 @@
+
+/*2. The Great Comparator
+
+Create a function that compares two integers, a and b, and returns a numerical value indicating if a is greater, less than, or equal to b. This function will be used as a helper function for future module challenges.
+
+Specs:
+
+Use assertions to validate that the parameters passed into the function are integers
+If a is grater than b, return a positive number
+If a is less than b, return a negative number
+If a and b are equal, return a zero
+Here's an expected function prototype:
+
+function compare(a, b);
+
+Comparator Stretch Goal
+
+Extend your function to accept Strings as well as Integers. The function will need to detect the type of parameter passed in and determine what logic will need to be performed. Assume that a and b will be both Strings or both Integers. Use the above Specs as a guideline for your implementation. The same function prototype will apply.*/
+
 var integers = process.argv.slice(2);
 var a = integers[0];
 var b = integers[1];
+
+function comparator(a,b){
+	
+	if(a===b){
+		return 0 ;
+	}
+	else if(a>b){
+		return 1;
+	}
+	else{
+		return -1;
+	}
+}
+console.log(comparator(a,b));
---
+++ week-01/day-3-functions+CSS/panama.js
@@ -0,0 +1,43 @@
+/*##3.  A man, a plan, a canal: Panama!  
+Create a function that accepts a string as input and returns a boolean true if it is a palindrome (same spelling forwards and backwards) and false if otherwise.  You may use built-in String and array methods within this function.  
+
+Here's an expected function prototype:
+ 
+`function isPalindrome(a);`
+
+Notice that the function begins with the word is.  Best practice reminds us to prefix any function with a boolean return with a 'has', 'is', or other similar deliniation.  
+
+Specs:
+
+* You may use array manipulation, String manipulation, or a combination of both
+* Only test using single words
+
+##Palindrome Stretch Goal
+Extend your function to successfully test phrases (without punctuation.)  In the test, blank spaces should not be considered in comparing the forwards and backwards forms of hte phrases.
+
+##Extra Stretchy Goal
+Alter your function to only use a single traversal (one loop) of the phrase using any built-in String or array methods EXCEPT reverse.  Hint: Research loops!*/
+
+var panama = process.argv.slice(2);
+var string = panama[0];
+
+
+function isPalindrome(a){
+
+	for(var i = 0; i < a.length; i++){
+
+	console.log(a[i]);
+		//a[a.length-1] return the string value no the index value
+		if(a[i] === a[a.length - i]){
+			console.log(a[a.length-1-i]);
+			console.log(a[i]);
+			a = true;
+		}
+	else
+	{
+		a = false;
+	}
+	return a;
+}
+}
+console.log(isPalindrome(string));
