a7973fdefd9c0e0608e6199c95d811e86f15e531
humberto J calero <Humberto@humbertos-MacBook-Pro.local>
2015-08-18 22:07:53 -0700

    before day4w2 notes

--- week-02/day-01-arrays+Iterators/dusk-iteratorsLab/starterCode/iterators.js
+++ week-02/day-01-arrays+Iterators/dusk-iteratorsLab/starterCode/iterators.js
@@ -10,13 +10,7 @@ var o_o = {
     //code here...
   },
   filter: function(list, callback) {
-    var output = [];
-      for (var i = 0; i < list.length; i++) {
-        if (callback(list[i])) {
-          output.push(list[i]);
-        }
-      }
-      return output;
+    //code here...
   }
 };
 
